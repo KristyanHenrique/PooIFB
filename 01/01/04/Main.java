dff85adc2ed307d47b01beac16101922210fad25
Kristyan Henrique <41088986+KristyanHenrique@users.noreply.github.com>
2023-03-22 00:25:39 -0300

    FIm da lista 1

---
+++ 01/01/04/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scan =  new Scanner(System.in);
+		System.out.printf("Insira 2 Valores\n");
+		
+        ArrayList<Integer> listaInts = new ArrayList<Integer>();
+        
+        for (int i=0;i<2 ;i++ ) {
+            listaInts.add(scan.nextInt());
+        }
+        
+        Collections.sort(listaInts);
+        System.out.printf("%d %d",listaInts.get(0),listaInts.get(1));
+	}
+}
