026f18e68777088a73b12c4bf3881e9bcb612329
Kristyan Henrique <41088986+KristyanHenrique@users.noreply.github.com>
2023-03-30 23:00:27 +0000

    fa

--- Resumão/01.java
+++
@@ -1,4 +0,0 @@
-/*
-1) Faça uma algoritmo que calcule o perímetro do triângulo. O programa deve receber 3 números inteiros como parâmetro e deve retornar o valor do perímetro.
-*/
-
---
+++ Resumão/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+/*
+1) Faça uma algoritmo que calcule o perímetro do triângulo. O programa deve receber 3 números inteiros como parâmetro e deve retornar o valor do perímetro.
+*/
+public class Main {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+
+        System
+        for(int i = 0; i>3; i++){
+            lista.add(scan.nextInt());
+        }
+
+        for(int i = 0; i>3; i++){
+            lista.get(i);
+        }
+        
+    }
+}
